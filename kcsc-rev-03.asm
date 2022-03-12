c69de658a872c4a238434f6a4420da91b8103f02
Duong Tuan Anh <74018499+dtuananh@users.noreply.github.com>
2022-03-13 03:29:43 +0700

    Update kcsc-rev-03.asm

--- kcsc-rev-03.asm
+++ kcsc-rev-03.asm
@@ -1,64 +1,72 @@
 section .data
-	msg1 db "Enter a string: "
+	msg1 db "Enter a string: ", 0h
 	len1 equ $- msg1
-	msg2 db "Length of string: "
+	msg2 db "Length of string: ", 0h
 	len2 equ $- msg2
-	count dw 0
-	size_count equ $- count
+	
 section .bss
 	string resb 255
-
+	count resb 1
+	
 section .text
 	global _start
 
 _start:
 	
-	; Displaying msg1
+; Displaying msg1
 	mov eax, 4
 	mov ebx, 1
 	mov ecx, msg1
 	mov edx, len1
 	int 0x80
 
-	; Read stdin
+; Read stdin
 	mov eax, 3
 	mov ebx, 0
 	mov ecx, string
 	mov edx, 255
 	int 0x80
 
-	; Displaying msg2
+; Displaying msg2
 	mov eax, 4
 	mov ebx, 1
 	mov ecx, msg2
 	mov edx, len2
 	int 0x80
 	
+	xor eax, eax		; eax = 0
+		
 	mov eax, string
 	call _strlen
+	mov [count], eax
 
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, count
-	mov edx, size_count
-	int 0x80
+	mov ecx, [count]
+	add ecx, 48		; convert to string
+	mov [count], ecx
 	
+; Print [count]	
+	mov edx, eax
+	mov ecx, count
+	mov ebx, 1
+	mov eax, 4
+	int 0x80
+
+; Exit	
 	mov eax, 1
-	mov ebx, 0
+	xor ebx, ebx
 	int 0x80
 
 _strlen:
-	push ebx
-	mov ebx, eax
-
-_nextChar:
-	cmp byte [eax], 0
-	jz _exit
-	inc eax
-	inc word [count]
-	jmp _nextChar
-	
-_exit:	
-	sub eax, ebx
-	pop ebx
-	ret
+    	push ebx
+    	mov  ebx, eax
+ 
+.nextchar:
+    	cmp byte [eax], 0xA
+    	jz  .finished
+    	inc eax
+    	jmp .nextchar
+ 
+.finished:
+    	sub eax, ebx
+    	pop ebx
+    	ret
