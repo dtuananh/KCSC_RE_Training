4f1a511a65d5f296f449e5fa2df4eec92fe7ca92
Duong Tuan Anh <74018499+dtuananh@users.noreply.github.com>
2022-03-20 21:15:40 +0700

    Update kcsc-rev-03.asm

--- kcsc-rev-03.asm
+++ kcsc-rev-03.asm
@@ -1,3 +1,5 @@
+%include 'funcs.asm'
+
 section .data
 	msg1 db "Enter a string: ", 0h
 	len1 equ $- msg1
@@ -14,59 +16,25 @@ section .text
 _start:
 	
 ; Displaying msg1
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, msg1
-	mov edx, len1
-	int 0x80
+	mov eax, msg1
+	call _sPrint
 
 ; Read stdin
-	mov eax, 3
-	mov ebx, 0
-	mov ecx, string
-	mov edx, 255
-	int 0x80
+	mov eax, string
+	call _scanStr
 
 ; Displaying msg2
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, msg2
-	mov edx, len2
-	int 0x80
-	
-	xor eax, eax		; eax = 0
-		
+	mov eax, msg2
+	call _sPrint
+
+; Calculated 	
+	xor eax, eax		; eax = 0	
 	mov eax, string
 	call _strlen
 	mov [count], eax
 
-	mov ecx, [count]
-	add ecx, 48		; convert to string
-	mov [count], ecx
+	xor eax, eax
+	mov eax, [count]
+	call _iPrint
 	
-; Print [count]	
-	mov edx, eax
-	mov ecx, count
-	mov ebx, 1
-	mov eax, 4
-	int 0x80
-
-; Exit	
-	mov eax, 1
-	xor ebx, ebx
-	int 0x80
-
-_strlen:
-    	push ebx
-    	mov  ebx, eax
- 
-.nextchar:
-    	cmp byte [eax], 0xA
-    	jz  .finished
-    	inc eax
-    	jmp .nextchar
- 
-.finished:
-    	sub eax, ebx
-    	pop ebx
-    	ret
+	call _quit
